8169b5b3aa4f8f839fd097a314d2dafa0bffa491
SrivastavArchit <architpulkit2003@gmail.com>
2024-04-07 01:21:33 +0530

    techstack

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Blank from '../components/Blank/Blank';
 import Hero from '../components/Hero/Hero';
 import Card from '../components/Cards/Card';
+import Techstack from '../components/techstack/Techstack';
+
 
 const Homepage = () => {
     return (
@@ -10,6 +12,8 @@ const Homepage = () => {
                 <Blank />
                 <Hero/>
                 <Card/>
+                <Techstack/>
+          
             </div>
         </>
     )
