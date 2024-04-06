feceb9148c47d9c415a6bbd096ab97d98c640c46
SrivastavArchit <architpulkit2003@gmail.com>
2024-04-06 16:50:51 +0530

    loader created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React,{ useEffect }from 'react';
+import React,{ useEffect, useState }from 'react';
 import {BrowserRouter,Route,Routes,useLocation} from 'react-router-dom';
 import Homepage from './Pages/Homepage';
 import Form from './Pages/Form';
 import Navbar from './components/Navbar/Navbar';
 import Aboutpage from './Pages/Aboutpage';
 import Contactpage from './Pages/Contactpage';
+import Preloader from './components/Preloader/Preloader';
+
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -16,22 +18,37 @@ function ScrollToTop() {
   return null;
 }
 
+
+
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    // Simulate content loading
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Change this value to the time you expect your content to load
+  }, []);
   return (
     <>
-      <BrowserRouter>
-          <Navbar/>
-            <ScrollToTop />
-            <Routes>
-                <Route path='/' element={<Homepage/>}/>
-                <Route path='/aboutpage' element={<Aboutpage/>}/>
-                <Route path='/contactpage' element={<Contactpage/>}/>
-                <Route path='/form' element={<Form/>}/>
-
-                
-                {/* <Route path='*' element={<Error/>}/> */}
-            </Routes>
-          </BrowserRouter> 
+    {
+      isLoading?(
+        <Preloader/>
+      ):(
+        <BrowserRouter>
+        <Navbar/>
+          <ScrollToTop />
+          <Routes>
+              <Route path='/' element={<Homepage/>}/>
+              <Route path='/aboutpage' element={<Aboutpage/>}/>
+              <Route path='/contactpage' element={<Contactpage/>}/>
+              <Route path='/form' element={<Form/>}/>
+             
+              {/* <Route path='*' element={<Error/>}/> */}
+          </Routes>
+        </BrowserRouter> 
+      )
+    }
+     
     </>
   )
 }
